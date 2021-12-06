db56a1818625fa2b9d359418b877aa788e1dcc95
Aaron Paden <aaronbpaden@gmail.com>
2021-12-06 09:08:21 -0600

    markup changes.

--- menu.php
+++ menu.php
@@ -1,16 +1,9 @@
 <?php
 require('templates/header.html');
 require('store-items.php');
-print('<main class="main container">');
+print('<div id="menuPage" class="container">');
 build_entree_list();
 build_side_list();
-/* print('<h2>Entrée (each comes with two free sides)</h2>'); */
-/* print('<div id="entreeMenu" class="menu-container">'); */
-/* print('</div>'); */
-/* print('<h2>Sides</h2>'); */
-/* print('<div id="sideMenu" class="menu-container">'); */
-/* print('</div>'); */
-/* print('<button id="checkoutButton" class="btn btn-primary checkout-button d-none">Checkout!</button>'); */
-print('</main>');
+print('</div>');
 require('templates/footer.html');
 ?>
